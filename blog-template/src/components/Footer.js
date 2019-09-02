045aec9aa7a56aaf8fd28a7e758b093ff310f7b6
Luis De Anda <luisdeanda916@outlook.com>
2019-09-02 12:41:46 -0700

    Social media links updated

--- blog-template/src/components/Footer.js
+++ blog-template/src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
             <div className="list-container">
                 <span className="footer-head">Social</span>
                 <div className="icon-container">
-                    <img src={require("../assets/instagram.svg")} className="social-icon" />
-                    <img src={require("../assets/twitter.svg")} className="social-icon" />
+                    <a href="https://www.instagram.com/code.with.luis/"><img src={require("../assets/instagram.svg")} alt="Instagram" className="social-icon" /></a>
+                    <a href="https://twitter.com/CodeWithLuis"><img src={require("../assets/twitter.svg")} alt="twitter" className="social-icon" /></a>
                 </div>
                 {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
                 {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
