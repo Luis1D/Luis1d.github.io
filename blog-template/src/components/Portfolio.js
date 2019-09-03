c3b816e6b40e84eeccb97e50ba4efb238214e31d
Luis De Anda <luisdeanda916@outlook.com>
2019-09-03 11:42:00 -0700

    Team Builder app added to portfolio

--- blog-template/src/components/Portfolio.js
+++ blog-template/src/components/Portfolio.js
@@ -4,9 +4,14 @@ const Portfolio = () => {
     return (
         <div className="portfolio-section display-item">
                 <div className="portfolio-cards">
-                    <h3 className="portfolio-title">React Todo App</h3>
-                    <a href="https://react-todo.luisdeanda.now.sh"><button className="portfolio-app">View App</button></a>
-                    <a href="https://github.com/Luis1D"><button className="portfolio-github">View On Github</button></a>
+                    <h3 className="portfolio-title">Todo App</h3>
+                    <a href="https://react-todo.luisdeanda.now.sh" target="_blank"><button className="portfolio-app">View App</button></a>
+                    <a href="https://github.com/Luis1D" target="_blank"><button className="portfolio-github">View On Github</button></a>
+                </div>
+                <div className="portfolio-cards">
+                    <h3 className="portfolio-title">Team Builder</h3>
+                    <a href="https://user-onboarding.luisdeanda.now.sh" target="_blank"><button className="portfolio-app">View App</button></a>
+                    <a href="https://github.com/Luis1D/User-Onboarding/tree/master/user-onboarding" target="_blank"><button className="portfolio-github">View On Github</button></a>
                 </div>
         </div>
     )
