ed6605bd810e242beac42bcedbf0c8679d1200a4
Luis De Anda <luisdeanda916@outlook.com>
2020-01-23 14:18:47 -0800

    updated layout in portfolio section

--- blog-template/src/components/Portfolio.js
+++ blog-template/src/components/Portfolio.js
@@ -9,54 +9,52 @@ import chartjs from '../assets/chartjs.jpg';
 const Portfolio = () => {
     return (
         <div className="portfolio-section display-item">
-                <div className="portfolio-cards">
-                    <h3 className="portfolio-title"> > Cryptocurrncy News Site</h3>
-                    <div className="preview-container cryptonews-app">
-                        <div className="tech-used">
-                        <h4>Tech Used:</h4>
-                            <table>
-                                <tr>
-                                    <th>Language</th>
-                                    <th>Frameworks</th>
-                                    <th>Styling</th>
-                                </tr>
-                                <tr>
-                                    <td>- JavaScript</td>
-                                    <td>- React.JS <br/> - Chart.JS</td>
-                                    <td>- SASS</td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div className="button-container">
-                            <a href="https://cryptonews-app.now.sh/" target="_blank" rel="noopener noreferrer">Visit App</a>
-                        </div>
-                    </div>
-
+            <div className="portfolio-cards">
+                <h3 className="portfolio-title"> > CryptoNewsAndData.com</h3>
+                <div className="preview-container cryptonews-app">
+                    <a href="https://cryptocurrency-news.now.sh/" target="_blank" rel="noopener noreferrer">
+                        <h3>Visit App</h3>
+                    </a>
                 </div>
-                <div className="portfolio-cards">
-                    <h3 className="portfolio-title"> > Todo App</h3>
-                    <div className="preview-container todo-app">
-                    <div className="tech-used">
-                        <h4>Tech Used:</h4>
-                            <table>
-                                <tr>
-                                    <th>Language</th>
-                                    <th>Frameworks</th>
-                                    <th>Styling</th>
-                                </tr>
-                                <tr>
-                                    <td>- JavaScript</td>
-                                    <td>- React.JS</td>
-                                    <td>- SASS</td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div className="button-container">
-                            <a href="https://todo-app.luisdeanda.now.sh/" target="_blank" rel="noopener noreferrer">Visit App</a>
-                        </div>
-                    </div>
-                    
+                <div className="tech-used">
+                    <h4>Tech Used:</h4>
+                    <table>
+                        <tr>
+                            <th>Language</th>
+                            <th>Frameworks</th>
+                            <th>Styling</th>
+                        </tr>
+                        <tr>
+                            <td>JavaScript</td>
+                            <td>React.JS <br/>Chart.JS</td>
+                            <td>SASS</td>
+                        </tr>
+                    </table>
                 </div>
+            </div>
+            <div className="portfolio-cards">
+                <h3 className="portfolio-title"> > Todo App</h3>
+                <div className="preview-container todo-app">
+                    <a href="https://todo-app.luisdeanda.now.sh/" target="_blank" rel="noopener noreferrer">
+                        <h3>Visit App</h3>
+                    </a>
+                </div>
+                <div className="tech-used">
+                    <h4>Tech Used:</h4>
+                    <table>
+                        <tr>
+                            <th>Language</th>
+                            <th>Frameworks</th>
+                            <th>Styling</th>
+                        </tr>
+                        <tr>
+                            <td>JavaScript</td>
+                            <td>React.JS</td>
+                            <td>SASS</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
