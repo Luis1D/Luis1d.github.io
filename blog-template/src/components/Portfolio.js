b3d1b4f155dfcf0138aaa7a0b138fee4599a79d1
Luis De Anda <luisdeanda916@outlook.com>
2020-01-23 18:47:41 -0800

    routes updated

--- blog-template/src/components/Portfolio.js
+++ blog-template/src/components/Portfolio.js
@@ -4,7 +4,7 @@ const Portfolio = () => {
     return (
         <div className="portfolio-section display-item">
             <div className="portfolio-cards">
-                <h3 className="portfolio-title"> > CryptoNewsAndData.com</h3>
+                <h3 className="portfolio-title"> > Crypto News And Data</h3>
                 <div className="preview-container cryptonews-app">
                     <a href="https://cryptocurrency-news.now.sh/" target="_blank" rel="noopener noreferrer">
                         <h3>Visit App</h3>
