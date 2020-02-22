42c540fe0d0cf5e81fbdbb943a3a3607dce45ac6
Luis De Anda <luisdeanda916@outlook.com>
2020-02-22 00:14:38 -0800

    project preview redone

--- blog-template/src/components/Portfolio.js
+++ blog-template/src/components/Portfolio.js
@@ -5,26 +5,72 @@ const Portfolio = () => {
         <div className="portfolio-section display-item">
             <div className="portfolio-cards">
                 <h3 className="portfolio-title"> > Crypto News And Data</h3>
-                <div className="preview-container cryptonews-app">
-                    <a href="https://cryptocurrency-news.now.sh/" target="_blank" rel="noopener noreferrer">
-                        <h3>Visit App</h3>
-                    </a>
+                <div className="preview-container">
+                    <div className="desktop-container">
+                        <div className="frame cryptonews">
+                            <a href="https://cryptocurrency-news.now.sh/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="legs"></div>
+                        <div className="bottom"></div>
+                    </div>
+                    <div className="mobile-container">
+                        <div className="top"></div>
+                        <div className="frame cryptonews">
+                            <a href="https://cryptocurrency-news.now.sh/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="bottom"></div>
+                    </div>
                 </div>
             </div>
             <div className="portfolio-cards">
                 <h3 className="portfolio-title"> > Reusablelighters.com</h3>
-                <div className="preview-container reusable-lighters">
-                    <a href="https://reusablelighters.com/" target="_blank" rel="noopener noreferrer">
-                        <h3>Visit App</h3>
-                    </a>
+                <div className="preview-container">
+                    <div className="desktop-container">
+                        <div className="frame reusable-lighters">
+                            <a href="https://reusablelighters.com/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="legs"></div>
+                        <div className="bottom"></div>
+                    </div>
+                    <div className="mobile-container">
+                        <div className="top"></div>
+                        <div className="frame reusablelighters">
+                            <a href="https://reusablelighters.com/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="bottom"></div>
+                    </div>
                 </div>
+                
             </div>
             <div className="portfolio-cards">
                 <h3 className="portfolio-title"> > Todo App</h3>
-                <div className="preview-container todo-app">
-                    <a href="https://todo-app.luisdeanda.now.sh/" target="_blank" rel="noopener noreferrer">
-                        <h3>Visit App</h3>
-                    </a>
+                <div className="preview-container">
+                    <div className="desktop-container">
+                        <div className="frame todo-app">
+                            <a href="https://todo-app.luisdeanda.now.sh/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="legs"></div>
+                        <div className="bottom"></div>
+                    </div>
+                    <div className="mobile-container">
+                        <div className="top"></div>
+                        <div className="frame todoapp-mobile">
+                            <a href="https://todo-app.luisdeanda.now.sh/" target="_blank" rel="noopener noreferrer">
+                                <h3>Visit App</h3>
+                            </a>
+                        </div>
+                        <div className="bottom"></div>
+                    </div>
                 </div>
             </div>
         </div>
